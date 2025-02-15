052b6710d36e8b8722c0a9abd737fdfc215caedc
sharathchandra07 <msharathchandra2005@gmail.com>
2025-02-15 19:30:34 +0530

    commmm

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.json({ success: true, message: "GET request successful!" });
 });
 
