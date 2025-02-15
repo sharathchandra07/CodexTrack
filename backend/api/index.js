d452960c0b7c7e9e1ed36b1f2c666058e67b4988
sharathchandra07 <msharathchandra2005@gmail.com>
2025-02-15 19:39:36 +0530

    ki

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,12 +12,12 @@ app.use(cors({
 app.use(express.json());
 
 // API Test Route
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ success: true, message: "API is running successfully!" });
 });
 
 // API Data Fetch Route
-app.post("/api/data", async (req, res) => {
+app.post("/data", async (req, res) => {
     const platform = req.body.id;
     const id = req.body.message;
 
