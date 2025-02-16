ac4cf0d1908753d7b242c365bdcf3f172475b31f
sharathchandra07 <msharathchandra2005@gmail.com>
2025-02-16 09:02:06 +0530

    as

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const app = express();
 app.use(cors(
     {
-        origin: "*",
+        origin: ["https://codex-track-2x18a3p58-sharath-chandras-projects-df63d9ae.vercel.app", "https://localhost:3000"],
         methods: ["POST", "GET"],
         credentials: true
     }
