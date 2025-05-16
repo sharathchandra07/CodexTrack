1913d2f0a9b4061d8350ee09ed89816c2ec1c396
sharathchandra07 <msharathchandra2005@gmail.com>
2025-05-16 12:09:06 +0530

    version-2.o

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const chromium = require("chrome-aws-lambda");
 
 const app = express();
